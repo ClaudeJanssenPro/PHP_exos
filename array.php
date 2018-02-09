3469df40c499dbe406c9ce25e793a0871fc2529c
ClaudeJanssenPro <claudejanssen.pro@gmail.com>
2018-02-09 16:18:14 +0100

    boucles

--- array.php
+++ array.php
@@ -1,24 +1,34 @@
 <!--
 https://github.com/becodeorg/Turing-promo-4/blob/master/parcours/2-PHP/Introduction-Exercices/php-array.md
-Crée un array représentant ta famille.
-Affiche son contenu avec la fonction print_r
-Crée un array décrivant tes plats préférés.
-Affiche son contenu avec la fonction print_r
-Crée un array listant tes films et séries préférés.
-En utilisant cet array, affiche uniquement ton film/série préféré (echo $pays[2];)
-
 -->
 
 <?php
   $family = array( 'Michel', 'Claudine' , 'Mathéo', 'Fabian', 'Raphaël' );
   $fav_food = array( 'Bolognaise', 'Poulet' , 'Pizza');
-  $fav_movie = array( 'Vikings', 'Star Trek' , 'The Orville', 'Star Wars');
+  $fav_movie = array( 'Vikings', 'Star Trek' , 'The Orville', 'Star Wars', 'Family Guy');
   $utilisateur = array(
     'prenom' => 'Juan',
     'nom' => 'Pablo',
     'adresse' => '3 Rue du Paradis',
     'ville' => 'Bruxelles'
     );
+  $papa = array(
+      'naissance'	=> 1961 ,
+      'prenom' 		=> 'Albert' ,
+      'nom' 	  		=> 'Janssen' ,
+      'aime_le_foot' => false ,
+      'hobbies' 	=> array( 'Chasse', 'Pêche' , 'Séries', 'Marche')
+    );
+  $moi = array(
+      'naissance'	=> "1979" ,
+      'prenom' 		=> 'Claude' ,
+      'nom' 	  		=> 'Janssen' ,
+      'aime_le_foot' => true ,
+      'hobbies' 	=> array( 'Lecture', 'Course' , 'Séries', 'PC') ,
+      'papa' => $papa
+    );
+$moi['hobbies'][] = 'Taxidermie';
+$moi['nom'] = 'Dieudonné';
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -35,6 +45,29 @@ En utilisant cet array, affiche uniquement ton film/série préféré (echo $pay
       print_r($fav_food);
       echo "<br />";
       echo $fav_movie[0];
+      echo "<br />";
+      echo $utilisateur['ville'];
+      echo "<br />";
+      echo "<br />";
+      echo '<pre>';
+      print_r($moi);
+      echo '</pre>';
+      echo '<br>';
+      echo $moi['nom'];
+      echo '<br>';
+      echo "# Hobbies de papa : "; echo count($papa['hobbies']);
+      echo '<br>';
+      echo "# Hobbies : "; echo count($moi['hobbies']);
+      echo '<br>';
+      echo "Somme : "; echo count($papa['hobbies'])+count($moi['hobbies']);
+      echo '<br>';
+      echo '<pre>';
+      echo "Hobby communs : "; print_r(array_intersect($papa['hobbies'],$moi['hobbies']));
+      echo '</pre>';
+      echo '<br>';
+      echo '<pre>';
+      echo "Nos hobbies : "; print_r(array_merge($papa['hobbies'],$moi['hobbies']));
+      echo '</pre>';
       ?>
     </body>
 </html>
---
+++ boucles_exos.php
@@ -0,0 +1,61 @@
+<?php
+$pronoms_personnels = array(
+  'Je' => 'code',
+  'Tu' => 'codes',
+  'Il/Elle' => 'code',
+  'Nous' => 'codons',
+  'Vous' => 'codez',
+  'Elles/Ils' => 'codent',
+);
+
+$web_development ['frontend'][] = 'xhtml';
+$web_development ['backend'][] = 'Ruby on Rails';
+$web_development ['frontend'][] = 'CSS';
+$web_development ['frontend'][] = 'Flash';
+$web_development ['frontend'][] = 'Javascript';
+$web_development['frontend']['0'] = 'html';
+$web_development['frontend']['0'] = 'html';
+array_splice($web_development['frontend'], 2, 1);
+$nombre_de_lignes = 1;
+$phrase = "Je ne dois pas regarder les mouches voler quand j'apprends le PHP";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Arrays</title>
+</head>
+    <body>
+      <?php
+      echo '<pre>';
+      foreach ($pronoms_personnels as $pro => $verbe) {
+          echo  $pro ,' '. $verbe . "<br />";
+      }
+      echo '</pre>';
+      echo "<br />";
+      echo '<pre>';
+      while ($nombre_de_lignes <= 100) {
+          echo $nombre_de_lignes ,' '. $phrase . "<br />";
+          $nombre_de_lignes++;
+      }
+      echo '</pre>';
+
+      for ($nombre_de_lignes = 1; $nombre_de_lignes <= 100; $nombre_de_lignes++) {
+          echo 'Ceci est la ligne n°' . $nombre_de_lignes . '<br />';
+      }
+      ?>
+    </body>
+</html>
+
+
+<!--
+https://github.com/becodeorg/Turing-promo-4/blob/master/parcours/2-PHP/Introduction-Exercices/php-boucles.md
+
+Ecris une boucle qui affiche les numéros de 1 à 120 à l'aide de  while
+Ecris une boucle qui affiche les numéros de 1 à 120 à l'aide de  for
+Crée un tableau contenant tous les prénoms des personnes composant ta classe. Affiche ces prénoms à l'aide d'une boucle.
+Crée un tableau contenant au moins 10 pays du monde. Une fois fait, utilise une boucle pour générer du html correspondant à une selectbox permettant à un utilisateur d'indiquer son pays dans un formulaire html.
+A présent, modifie ton tableau contenant les pays pour qu'il soit un tableau associatif: la clef est le code ISO du pays, la valeur étant le nom du pays: par exemple: $pays = array('BE'=>'Belgique'); au lieu de simplement $pays = array('Belgique');. Utilise la clef pour qu'elle devienne la valeur de la balise option dans ton html.
+-->
