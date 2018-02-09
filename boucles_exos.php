7e72b139b1861429a30b18b6e9929d95345402a9
ClaudeJanssenPro <claudejanssen.pro@gmail.com>
2018-02-09 16:32:26 +0100

    b4we

--- boucles_exos.php
+++ boucles_exos.php
@@ -34,17 +34,21 @@ $phrase = "Je ne dois pas regarder les mouches voler quand j'apprends le PHP";
           echo  $pro ,' '. $verbe . "<br />";
       }
       echo '</pre>';
+      echo "While x 120";
       echo "<br />";
-      echo '<pre>';
-      while ($nombre_de_lignes <= 100) {
-          echo $nombre_de_lignes ,' '. $phrase . "<br />";
+      echo '<pre>' ;
+      while ($nombre_de_lignes <= 120) {
+        echo 'Ceci est la ligne While n°' . $nombre_de_lignes . "<br />";
           $nombre_de_lignes++;
       }
       echo '</pre>';
-
-      for ($nombre_de_lignes = 1; $nombre_de_lignes <= 100; $nombre_de_lignes++) {
-          echo 'Ceci est la ligne n°' . $nombre_de_lignes . '<br />';
+      echo "For x 120";
+      echo "<br />";
+      echo '<pre>' ;
+      for ($nombre_de_lignes = 1; $nombre_de_lignes <= 120; $nombre_de_lignes++) {
+        echo 'Ceci est la ligne For n°' . $nombre_de_lignes . "<br />";
       }
+      echo '</pre>';
       ?>
     </body>
 </html>
@@ -53,8 +57,6 @@ $phrase = "Je ne dois pas regarder les mouches voler quand j'apprends le PHP";
 <!--
 https://github.com/becodeorg/Turing-promo-4/blob/master/parcours/2-PHP/Introduction-Exercices/php-boucles.md
 
-Ecris une boucle qui affiche les numéros de 1 à 120 à l'aide de  while
-Ecris une boucle qui affiche les numéros de 1 à 120 à l'aide de  for
 Crée un tableau contenant tous les prénoms des personnes composant ta classe. Affiche ces prénoms à l'aide d'une boucle.
 Crée un tableau contenant au moins 10 pays du monde. Une fois fait, utilise une boucle pour générer du html correspondant à une selectbox permettant à un utilisateur d'indiquer son pays dans un formulaire html.
 A présent, modifie ton tableau contenant les pays pour qu'il soit un tableau associatif: la clef est le code ISO du pays, la valeur étant le nom du pays: par exemple: $pays = array('BE'=>'Belgique'); au lieu de simplement $pays = array('Belgique');. Utilise la clef pour qu'elle devienne la valeur de la balise option dans ton html.
